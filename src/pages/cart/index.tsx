187334832b26354a8affeee3746b9882fc42434e
HOANGNBD <Duchoang020120@gmail.com>
2023-06-14 18:48:10 +0700

    update

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -12,10 +12,12 @@ import {
   Menu,
 } from "antd";
 import CreateOrder from "@/compenents/ShopApp/components/Order/CreateOrder";
+import { useAuthStore } from "@/hook/useAuthStore";
 const URL_ENV = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:9000";
 
 export default function CounterApp() {
   const [loading, setLoading] = useState(true);
+  const { auth } = useAuthStore((state: any) => state);
   const { selectAllCheck, removeAllCheck, itemsCheckout } = useCartStore(
     (state: any) => state
   );
@@ -40,108 +42,119 @@ export default function CounterApp() {
   }, []);
   return (
     <div>
-      <div style={{ background: "rgb(245,245,245)" }}>
-        <h4 className="text-center py-4">Giỏ hàng của bạn</h4>
-      </div>
+      {auth?.payload ? (
+        <div style={{ background: "rgb(245,245,245)" }}>
+          <h4 className="text-center py-4">Giỏ hàng của bạn</h4>
+        </div>
+      ) : (
+        <div style={{ background: "rgb(245,245,245)" }}>
+          <h4 className="text-center py-4">
+            Vui lòng đăng nhập để truy cập giỏ hàng
+          </h4>
+        </div>
+      )}
 
-      <Card className="bg-body-secondary" loading={loading}>
-        <ShopApp />
-        <Affix offsetBottom={10}>
-          <Card
-            type="inner"
-            title={
-              <Divider orientation="left">Tạm tính đơn hàng của bạn</Divider>
-            }
-            className="container"
-          >
-            <div className="row text-center">
-              <div className="col-lg-2">
-                <Checkbox
-                  onChange={(info: any) => {
-                    if (info.target.checked) {
-                      selectAllCheck();
-                    } else {
-                      removeAllCheck();
-                    }
-                  }}
-                >
-                  {" "}
-                  Chọn tất cả
-                </Checkbox>
-              </div>
-              <div className="col-lg-4">
-                <Dropdown
-                  trigger={windowWidth < 900 ? ["contextMenu"] : ["hover"]}
-                  className="text-center"
-                  overlayStyle={{
-                    zIndex: 100000000000,
-                  }}
-                  placement="topRight"
-                  menu={{
-                    items: itemsCheckout.map((item: any, index: number) => ({
-                      key: index.toString(),
-                      label: (
-                        <div
-                          onClick={(e) => e.preventDefault()}
-                          className="d-flex justify-content-between  "
-                        >
-                          <div className="w-75 text-truncate py-3">
-                            <span> {item.product?.name}</span>
-                          </div>
+      {auth?.payload && (
+        <Card className="bg-body-secondary" loading={loading}>
+          <ShopApp />
+          <Affix offsetBottom={10}>
+            <Card
+              type="inner"
+              title={
+                <Divider orientation="left">Tạm tính đơn hàng của bạn</Divider>
+              }
+              className="container"
+            >
+              <div className="row text-center">
+                <div className="col-lg-2">
+                  <Checkbox
+                    onChange={(info: any) => {
+                      if (info.target.checked) {
+                        selectAllCheck();
+                      } else {
+                        removeAllCheck();
+                      }
+                    }}
+                  >
+                    {" "}
+                    Chọn tất cả
+                  </Checkbox>
+                </div>
+                <div className="col-lg-4">
+                  <Dropdown
+                    trigger={windowWidth < 900 ? ["contextMenu"] : ["hover"]}
+                    className="text-center"
+                    overlayStyle={{
+                      zIndex: 100000000000,
+                    }}
+                    placement="topRight"
+                    menu={{
+                      items: itemsCheckout.map((item: any, index: number) => ({
+                        key: index.toString(),
+                        label: (
+                          <div
+                            onClick={(e) => e.preventDefault()}
+                            className="d-flex justify-content-between  "
+                          >
+                            <div className="w-75 text-truncate py-3">
+                              <span> {item.product?.name}</span>
+                            </div>
 
-                          <div className="py-3">
-                            {item.product?.price?.toLocaleString("vi-VN", {
-                              style: "currency",
-                              currency: "VND",
-                            })}
+                            <div className="py-3">
+                              {item.product?.price?.toLocaleString("vi-VN", {
+                                style: "currency",
+                                currency: "VND",
+                              })}
+                            </div>
                           </div>
+                        ),
+                        icon: (
+                          <Badge color="blue" count={item.quantity}>
+                            <Avatar
+                              shape="square"
+                              size="large"
+                              src={`${URL_ENV}${item.product?.imageUrl}`}
+                            />
+                          </Badge>
+                        ),
+                      })),
+                    }}
+                  >
+                    <div className="text-center">
+                      <Badge
+                        className="text-center"
+                        count={itemsCheckout?.length}
+                      >
+                        <div className="py-1 text-center">
+                          Tổng số sản phẩm đã chọn
                         </div>
-                      ),
-                      icon: (
-                        <Badge color="blue" count={item.quantity}>
-                          <Avatar
-                            shape="square"
-                            size="large"
-                            src={`${URL_ENV}${item.product?.imageUrl}`}
-                          />
-                        </Badge>
-                      ),
-                    })),
-                  }}
-                >
-                  <div className="text-center">
-                    <Badge
-                      className="text-center"
-                      count={itemsCheckout?.length}
-                    >
-                      <div className="py-1 text-center">
-                        Tổng số sản phẩm đã chọn
-                      </div>
-                    </Badge>
-                  </div>
-                </Dropdown>
+                      </Badge>
+                    </div>
+                  </Dropdown>
+                </div>
+                <div className="col-lg-4">
+                  Tổng thanh toán :{" "}
+                  {itemsCheckout
+                    .map((item: any) => item.product.price * item.quantity)
+                    .reduce(
+                      (accumulator: any, subtotal: any) =>
+                        accumulator + subtotal,
+                      0
+                    )
+                    .toLocaleString("vi-VN", {
+                      style: "currency",
+                      currency: "VND",
+                    })}
+                </div>
+                <div className="col-lg-2">
+                  {" "}
+                  <CreateOrder />
+                </div>
               </div>
-              <div className="col-lg-4">
-                Tổng thanh toán :{" "}
-                {itemsCheckout
-                  .map((item: any) => item.product.price * item.quantity)
-                  .reduce(
-                    (accumulator: any, subtotal: any) => accumulator + subtotal,
-                    0
-                  )
-                  .toLocaleString("vi-VN", {
-                    style: "currency",
-                    currency: "VND",
-                  })}
-              </div>
-              <div className="col-lg-2">
-                {" "}
-                <CreateOrder />
-              </div>
-            </div>
-          </Card>
-        </Affix>
-      </Card>
+            </Card>
+          </Affix>
+        </Card>
+      )}
     </div>
   );
 }
